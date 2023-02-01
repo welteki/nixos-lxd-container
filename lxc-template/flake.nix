ac6e3b86c07a08943b14418eafeb616319c4483a
Vladimír Jeřábek <jerab.vl@gmail.com>
2023-02-01 11:38:37 +0100

    refactor: extract configuration to lxc-container.nix

--- lxc-template/flake.nix
+++ lxc-template/flake.nix
@@ -1,26 +1,14 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
   };
 
   outputs = { self, nixpkgs, ... }: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
-        # Include the default lxd configuration.
+        # Include the default lxc configuration.
         ./lxc-container.nix
-
-        ({ pkgs, ... }: {
-          # Install flakes
-          nix.package = pkgs.nixUnstable;
-          nix.extraOptions = ''
-            experimental-features = nix-command flakes ca-references
-          '';
-
-          # Networking
-          networking.useDHCP = false;
-          networking.interfaces.eth0.useDHCP = true;
-        })
       ];
     };
   };
