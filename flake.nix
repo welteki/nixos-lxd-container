6ab1707611941f5ee60bbea7a4f538c7b8813cfb
Vladimír Jeřábek <jerab.vl@gmail.com>
2023-02-01 11:38:06 +0100

    refactor: update nixpkgs version and extract some configuration to lxc-container.nix

--- flake.nix
+++ flake.nix
@@ -1,23 +1,15 @@
 {
   description = "NixOS lxd container configuration";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.05";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
 
   outputs = { self, nixpkgs }: {
     nixosConfigurations.lxc = nixpkgs.lib.makeOverridable nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules =[
+        ./lxc-template/lxc-container.nix
+
         ({ pkgs, ... }: {
-          imports = [
-            ./lxc-template/lxc-container.nix
-          ];
-
-          # Install flakes
-          nix.package = pkgs.nixUnstable;
-          nix.extraOptions = ''
-            experimental-features = nix-command flakes ca-references
-          '';
-
           # Copy the config for nixos-rebuild
           system.activationScripts.config = ''
             if [ ! -e /etc/nixos/flake.nix ]; then
@@ -26,16 +18,12 @@
               cat ${./lxc-template/flake.nix} > /etc/nixos/flake.nix
             fi
           '';
-          
+
           # Make lxc exec work properly
           system.activationScripts.bash = ''
             mkdir -p /bin
             ln -sf /run/current-system/sw/bin/bash /bin/bash
           '';
-
-          # Network
-          networking.useDHCP = false;
-          networking.interfaces.eth0.useDHCP = true;
         })
       ];
     };
